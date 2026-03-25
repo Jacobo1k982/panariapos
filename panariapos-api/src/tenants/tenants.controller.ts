74baabc3e4ac84cee659d4c524ceba520c080212
root <root@srv1374014.hstgr.cloud>
2026-03-25 17:05:03 +0000

    fix: register endpoint, dockerfile build args

--- panariapos-api/src/tenants/tenants.controller.ts
+++ panariapos-api/src/tenants/tenants.controller.ts
@@ -1,25 +1,28 @@
-import { Controller, Get, Patch, Body, UseGuards } from '@nestjs/common'
-import { TenantsService, UpdateTenantDto }          from './tenants.service'
-import { JwtAuthGuard }                             from '../auth/guards/jwt.guard'
-import { RolesGuard }                               from '../auth/guards/roles.guard'
-import { Roles }                                    from '../auth/decorators/roles.decorator'
-import { CurrentUser }                              from '../auth/decorators/current-user.decorator'
-import { Role }                                     from '@prisma/client'
+import { Controller, Get, Patch, Post, Body, UseGuards } from '@nestjs/common'
+import { TenantsService, UpdateTenantDto, RegisterTenantDto } from './tenants.service'
+import { JwtAuthGuard }  from '../auth/guards/jwt.guard'
+import { RolesGuard }    from '../auth/guards/roles.guard'
+import { Roles }         from '../auth/decorators/roles.decorator'
+import { CurrentUser }   from '../auth/decorators/current-user.decorator'
+import { Role }          from '@prisma/client'
 
-@UseGuards(JwtAuthGuard)
 @Controller('tenants')
 export class TenantsController {
   constructor(private tenants: TenantsService) {}
 
-  // Obtener datos del tenant actual
+  @Post('register')
+  register(@Body() dto: RegisterTenantDto) {
+    return this.tenants.register(dto)
+  }
+
+  @UseGuards(JwtAuthGuard)
   @Get('me')
   getMe(@CurrentUser() u: any) {
     return this.tenants.findOne(u.tenantId)
   }
 
-  // Actualizar datos del tenant
   @Roles(Role.ADMIN)
-  @UseGuards(RolesGuard)
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Patch('me')
   updateMe(@CurrentUser() u: any, @Body() dto: UpdateTenantDto) {
     return this.tenants.update(u.tenantId, dto)
